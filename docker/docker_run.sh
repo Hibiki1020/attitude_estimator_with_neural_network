dcfc1b0994cfc02fd306e7660270e79826cc1706
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-08 12:05:27 +0900

    add kawai_image_to_gravity

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -15,7 +15,7 @@ docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --net=host \
-    -v /home/kawai/airsim_dataset:/home/ssd_dir \
+    -v /media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21:/home/ssd_dir \
 	-v /home/kawai/num_regression_attitude_estimator_log:/home/num_regression_attitude_estimator_log \
 	-v /home/kawai/deep_visual_gravity_vector_detection_for_unmanned_aircraft_attitude_estimation_log:/home/deep_visual_gravity_vector_detection_for_unmanned_aircraft_attitude_estimation_log \
 	$image_name:$tag_name
