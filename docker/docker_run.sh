7d084ea7b8a0659980ecc376d58088e880aa4ac0
Hibiki Kawai <hibikijitaku@gmail.com>
2021-08-22 16:15:41 +0900

    SII 2022

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -6,6 +6,7 @@ root_path=$(pwd)
 
 # /media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21
 # is ssd path in author's environment
+# -v /home/kawai/airsim_dataset:/home/ssd_dir \
 
 xhost +
 docker run -it --rm \
@@ -15,7 +16,7 @@ docker run -it --rm \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --net=host \
-    -v /home/kawai/airsim_dataset:/home/ssd_dir \
+    -v /media/amsl/96fde31e-3b9b-4160-8d8a-a4b913579ca21:/home/ssd_dir \
 	-v /home/kawai/num_regression_attitude_estimator_log:/home/num_regression_attitude_estimator_log \
 	-v /home/kawai/deep_visual_gravity_vector_detection_for_unmanned_aircraft_attitude_estimation_log:/home/deep_visual_gravity_vector_detection_for_unmanned_aircraft_attitude_estimation_log \
 	$image_name:$tag_name
